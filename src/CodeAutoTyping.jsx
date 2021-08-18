459f6c40de22ea92766e9833b6f1324c09060140
Cristian Azócar <caam.1429@gmail.com>
2021-08-18 14:22:09 -0400

    Create CodeAutoTyping component

---
+++ src/CodeAutoTyping.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typist from 'react-typist';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import './CodeAutoTyping.css';
+
+const defaultTypistProps = {
+  cursor: { show: false },
+};
+
+function CodeAutoTyping(props) {
+  const { children, syntaxHighlighterProps, ...rest } = props;
+
+  return (
+    <Typist {...defaultTypistProps} {...rest}>
+      <SyntaxHighlighter {...syntaxHighlighterProps}>
+        {children}
+      </SyntaxHighlighter>
+    </Typist>
+  );
+}
+
+CodeAutoTyping.propTypes = {
+  children: PropTypes.node,
+  syntaxHighlighterProps: PropTypes.instanceOf(SyntaxHighlighter),
+};
+
+CodeAutoTyping.defaultProps = {
+  children: undefined,
+  syntaxHighlighterProps: undefined,
+};
+
+export default CodeAutoTyping;
